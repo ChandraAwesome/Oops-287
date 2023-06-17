64cea2ab871569bb25d3ca9c0a0b87c30f55ae1c
ChandarAwesome <ajaygames00Gmail.com>
2023-06-17 10:31:45 +0530

    [Chandra]-Oops Programs

---
+++ Oops.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_programs
+{
+    public class Oops // created class with public access modifier// base class
+    {
+        public void createObject() // public void method
+        {
+            string name = "Ajay"; // create a string variable with value
+            int number = 10;
+            double value = 10.5455;
+            bool say = false;
+            char letter = 'A';
+            Console.WriteLine("Created the class and object "+name);
+        }
+    }
+
+    public class inheritance : Oops //derived class// inherit from the base class Oops
+    {
+        public string lastname = "Chandra"; // instance
+    }
+
+    public class Polymorphism1
+    {
+        public static void method() // static method
+        {
+            Console.WriteLine("Main Class");
+        }
+    }
+
+    public class Poly2 : Polymorphism1 // inherit from Polymorphism1 class to child class Poly2
+    {
+        public virtual void method() // virtual method for method overriding
+        {
+            Console.WriteLine("Polyclass");
+        }
+    }
+
+    class Ploy3 : Poly2
+    {
+        public override void method() // overridden method
+        {
+            Console.WriteLine("This is override method");
+        }
+    }
+
+    abstract class Name
+    {
+        public abstract void method1();
+
+        public static void method2()
+        {
+            Console.WriteLine("Abstraction");
+        }
+    }
+
+    class Name1 : Name
+    {
+        public override void method1()
+        {
+            Console.WriteLine("Overridden");
+        }
+    }
+
+    interface Iname 
+    {
+        public void method3(int number2);
+    }
+    interface Iname1
+    {
+        void method4();
+    }
+
+    class interfacess : Iname,Iname1
+    {
+        int number = 20; //value type
+        public int number2;
+        public void method3(int number2)
+        {
+            this.number2= number2; // reference type
+            Console.WriteLine("interface method3");
+        }
+
+        public void method4()
+        {
+            Console.WriteLine("interface method 4");
+        }
+    }
+
+    class modifiers
+    {
+        public int number = 10; // can access anywhere
+        private int number2;// can access only inside the same class
+        protected int number3 = 30; // can access only same class as well as the inherited class
+
+
+        public modifiers( int number2) //constructor and parameterized constructor
+        {
+            this.number2 = number2;
+            Console.WriteLine(number+number2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks.Dataflow;
+using OOPS_programs;
+namespace OOPS_programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oops Concepts \n");
+
+            Oops objects = new Oops(); // create object // because of it is non static method
+            objects.createObject();
+            Console.WriteLine(objects.createObject);
+            //inheritance
+            inheritance obj = new inheritance();
+            obj.createObject();
+            Console.WriteLine(obj.lastname);
+            //Polymorphism
+            Polymorphism1.method();
+            Poly2 obj3 = new Ploy3();
+            obj3.method();
+           // Abstraction
+            Name1 name1= new Name1();
+            name1.method1();
+            Name1.method2();
+            //interfaces 
+            interfacess obj5= new interfacess();
+            obj5.method3(5);
+            obj5.method4();
+            
+        }
+    }
+}
